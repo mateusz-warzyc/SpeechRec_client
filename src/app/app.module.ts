e95585d6cf0d721fd6e95ff05ab6aaacf5879234
mateusz-warzyc <mateuszw19911@wp.pl>
2017-09-06 17:23:44 +0200

    basic routing and login form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastModule } from 'ng2-toastr';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
-    ToastModule.forRoot()
+    ToastModule.forRoot(),
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path: '**', redirectTo: ''}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
